dae5bdb953499d8f7999ce65282baece4b71b31c
koreamarin <awldnjs2@gmail.com>
2024-10-10 19:53:00 +0900

    섹션4 객체 지향 패러다임

---
+++ src/main/java/cleancode/minesweeper/tobe/AnotherGame.java
@@ -0,0 +1,11 @@
+package cleancode.minesweeper.tobe;
+
+import cleancode.minesweeper.tobe.Game.GameRunnable;
+
+public class AnotherGame implements GameRunnable {
+
+    @Override
+    public void run() {
+        // 게임 진행
+    }
+}
---
+++ src/main/java/cleancode/minesweeper/tobe/Game/GameRunnable.java
@@ -0,0 +1,7 @@
+package cleancode.minesweeper.tobe.Game;
+
+public interface GameRunnable {
+
+    void run();
+
+}
